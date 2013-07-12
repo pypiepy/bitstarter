ce64b666dfe1c1e4b7cd4c2df94d4b09fd522cd5
Andrew <pypiepy@gmail.com>
2013-07-12 14:58:49 +0000

    Updated - web.js

--- web.js
+++ web.js
@@ -2,9 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buf = fs.readFileSync('./index.html');
-
 app.get('/', function(request, response) {
+  var buf = fs.readFileSync('./index.html', 'utf8');
   response.send(buf.toString('utf-8'));
 });
 
