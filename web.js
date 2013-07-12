c053f32a6b62d6f0ffa5435392c4d39c1de19f9b
Andrew <pypiepy@gmail.com>
2013-07-12 14:30:55 +0000

    Updated - second edit

--- web.js
+++ web.js
@@ -2,10 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var index = fs.readFileSync(index.html).toString();
+var buf = fs.readFileSync(index.html);
 
 app.get('/', function(request, response) {
-  response.send(index);
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
